380ecabf7458987ef6f4c0c8ec79c9940aeb004d
Tamir Mandreev <tamirmandreev@mail.ru>
2025-01-18 13:51:56 +0800

    Внес настройки в основной файл конфигурации settings.py для работы с PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'online_store_2',
+        'USER': 'tamir',
+        'PASSWORD': '14032712',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
