2d020a8e573c86c4484679afef2db3ede55e496d
Tamir Mandreev <tamirmandreev@mail.ru>
2025-01-22 18:16:00 +0800

    Добавил в шаблон главной страницы home.html код для отображения списка товаров с помощью цикла

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Product
+
+
 # Create your views here.
 
+# Создать контроллер (представление) домашней страницы
 def home(request):
-    return render(request, 'catalog/home.html')
+    # Получить все объекты модели Product
+    products = Product.objects.all()
+    # Сформировать контекст для передачи переменных в шаблон
+    context = {
+       'products': products
+    }
+    # Возвратить клиенту отрендеренную HTML-страницу
+    return render(request, 'catalog/home.html', context=context)
 
 def contacts(request):
     if request.method == 'POST':
