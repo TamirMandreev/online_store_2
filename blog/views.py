c282f4e2bc13532d9e35e1b9bab50f5361da2634
Tamir Mandreev <tamirmandreev@mail.ru>
2025-02-10 15:49:18 +0800

    Создал представление ArticleDeleteView для удаления объекта модели Article

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # ListView упрощает отображение списка объектов модели
 # DetailView отображает подробную информацию об объекте модели
 # UpdateView используется для обновления существующего объекта
-from django.views.generic import CreateView, ListView, DetailView, UpdateView
+# DeleteView используется для удаления существующего объекта
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from .models import Article
 
@@ -50,3 +51,14 @@ class ArticleUpdateView(UpdateView):
     template_name = 'article/article_form.html'
     # Указать маршрут, куда пользователь будет перенаправлен после успешного изменения объекта
     success_url = '#'
+
+# Создать представление для удаления объекта модели Article
+class ArticleDeleteView(DeleteView):
+    # Указать модель, с которой будет работать представление
+    model = Article
+    # Указать шаблон, который будет отображать страницу удаления объекта
+    template_name = 'article/article_confirm_delete.html'
+    # Задать имя переменной, под которой объект будет доступен в шаблоне
+    context_object_name = 'article'
+    # Указать маршрут, куда пользователь будет перенаправлен после успешного удаления объекта
+    success_url = 'article/home.html'
