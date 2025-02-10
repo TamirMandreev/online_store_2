9c411ef95240cc318ba517b93ce7810b419e17c5
Tamir Mandreev <tamirmandreev@mail.ru>
2025-02-10 10:29:00 +0800

    Создал представление ArticleListView для отображения списка объектов модели Article

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 # CreateView используется для создания нового объекта
-from django.views.generic import CreateView
+# ListView упрощает отображение списка объектов модели
+from django.views.generic import CreateView, ListView
 
 from .models import Article
 
 
 # Create your views here.
 
-# Создать представление для создания объекта
+# Создать представление для создания объекта модели Article
 class ArticleCreateView(CreateView):
     # Указать модель, с которой будет работать представление
     model = Article
@@ -17,4 +18,13 @@ class ArticleCreateView(CreateView):
     template_name = 'article/article_form.html'
     # Определить маршрут, куда пользователь будет перенаправлен после успешного создания объекта
     success_url = '#'
+
+# Создать представление для отображения списка объектов модели Article
+class ArticleListView(ListView):
+    # Указать модель, с которой будет работать представление
+    model = Article
+    # Указать шаблон, который будет использоваться для отображения списка объектов модели Article
+    template_name = 'article/article_list.html'
+    # Задать имя переменной, под которой список объектов будет доступен в шаблоне
+    context_object_name = 'articles'
 
