f8dfb3ed62dbb70b305355f2609ad5bb981db09e
Tamir Mandreev <tamirmandreev@mail.ru>
2025-02-10 10:15:30 +0800

    Создал представление ArticleCreateView для создания объектов модели Article

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+# CreateView используется для создания нового объекта
+from django.views.generic import CreateView
+
+from .models import Article
+
 
 # Create your views here.
+
+# Создать представление для создания объекта
+class ArticleCreateView(CreateView):
+    # Указать модель, с которой будет работать представление
+    model = Article
+    # Определить поля, которые будут отображаться в форме при создании объекта
+    fields = ['title', 'content', 'image',]
+    # Указать шаблон, который будет отображать форму создания объекта
+    template_name = 'article/article_form.html'
+    # Определить маршрут, куда пользователь будет перенаправлен после успешного создания объекта
+    success_url = '#'
+
