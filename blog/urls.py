cb382025f063c4c8dca8e78cf41a337175be55d4
Tamir Mandreev <tamirmandreev@mail.ru>
2025-02-10 14:53:38 +0800

    Создал маршрут для представления ArticleUpdateView

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('blog/create/', views.ArticleCreateView.as_view(), name='blog_create'),
     path('blog/', views.ArticleListView.as_view(), name='blog_list'),
     path('blog/detail/<int:pk>/', views.ArticleDetailView.as_view(), name='blog_detail'),
+    path('blog/update/<int:pk>/', views.ArticleUpdateView.as_view(), name='blog_update'),
 ]
