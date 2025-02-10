2635c50e05ea26ba922784e58a093b6aad7442f3
Tamir Mandreev <tamirmandreev@mail.ru>
2025-02-10 11:46:16 +0800

    Создал маршрут для представления ArticleDetailView

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 urlpatterns = [
     path('blog/create/', views.ArticleCreateView.as_view(), name='blog_create'),
     path('blog/', views.ArticleListView.as_view(), name='blog_list'),
+    path('blog/detail/<int:pk>/', views.ArticleDetailView.as_view(), name='blog_detail'),
 ]
