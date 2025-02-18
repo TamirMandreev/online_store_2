02e25459c04ccebd668bab9a5ade82101e4a5093
Tamir Mandreev <tamirmandreev@mail.ru>
2025-02-18 17:08:06 +0800

    Добавил маршрут для представления ProductDeleteView

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('product/detail/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
     path('product/create/', views.ProductCreateView.as_view(), name='product_create'),
     path('product/update/<int:pk>/', views.ProductUpdateView.as_view(), name='product_update'),
+    path('product/delete/<int:pk>/', views.ProductDeleteView.as_view(), name='product_delete'),
 ]
