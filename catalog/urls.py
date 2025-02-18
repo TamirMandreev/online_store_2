6f3afd1b62e63826bcf3247e89c14462712389c1
Tamir Mandreev <tamirmandreev@mail.ru>
2025-02-18 10:48:11 +0800

    Создал маршрут для ProductCreateView

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.ProductListView.as_view(), name='home'),
     path('contacts/', views.ContactsView.as_view(), name='contacts'),
     path('product_detail/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('product_create/', views.ProductCreateView.as_view(), name='product_create'),
 ]
