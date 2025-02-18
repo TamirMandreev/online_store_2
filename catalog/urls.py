e9e4398ee54abc21cf58a71e6c59ded98f6f726c
Tamir Mandreev <tamirmandreev@mail.ru>
2025-02-18 16:27:32 +0800

    Добавил маршрут для представления ProductUpdateView

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='home'),
     path('contacts/', views.ContactsView.as_view(), name='contacts'),
-    path('product_detail/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
-    path('product_create/', views.ProductCreateView.as_view(), name='product_create'),
+    path('product/detail/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('product/create/', views.ProductCreateView.as_view(), name='product_create'),
+    path('product/update/<int:pk>/', views.ProductUpdateView.as_view(), name='product_update'),
 ]
