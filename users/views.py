669e7c3d665783b92705d82b748945444208b02c
Tamir Mandreev <tamirmandreev@mail.ru>
2025-03-04 20:57:26 +0800

    Реализовал отправку приветственного письма пользователю после регистрации

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
+from django.core.mail import send_mail
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
 # CreateView предоставляет простой способ создания новой записи в базе данных через веб-интерфейс
 from django.views.generic import CreateView
 
+from config.settings import EMAIL_HOST_USER
 from users.forms import UserRegisterForm
 from users.models import User
 
@@ -20,3 +22,15 @@ class UserCreateView(CreateView):
     form_class = UserRegisterForm
     # Определить URL-адрес, на который будет перенаправлен пользователь после успешной отправки формы
     success_url = reverse_lazy('login')
+
+    # Реализовать отправку приветственного письма пользователю после регистрации
+    def form_valid(self, form):
+        # Создать объект на основе данных формы регистрации
+        user = form.save()
+        # Отправить приветственное письмо пользователю
+        send_mail(subject='Успешная регистрация',
+                  message='Спасибо, что зарегистрировались на нашем сайте',
+                  from_email=EMAIL_HOST_USER,
+                  recipient_list=[user.email],)
+
+        return super().form_valid(form)
