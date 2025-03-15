3cc26ee15a753a3a3e9de4aeb52832cba334d7a3
Tamir Mandreev <tamirmandreev@mail.ru>
2025-03-16 04:29:27 +0800

    Добавил низкоуровневое кэширование для сервисной функции get_products_from_cache

--- catalog/services.py
+++ catalog/services.py
@@ -14,7 +14,29 @@ def get_products_from_cache(category_name):
     '''
     # Получить категорию
     category = get_object_or_404(Category, name=category_name)
-    # Получить список всех продуктов из категории
-    products_list_by_category = Product.objects.filter(category=category)
-    # Возвратить список продуктов
-    return products_list_by_category
+
+    # Если кэширование выключено
+    if not CACHE_ENABLED:
+        # Получить список всех продуктов по категории из базы данных
+        products_list_by_category = Product.objects.filter(category=category)
+        # Возвратить список продуктов
+        return products_list_by_category
+
+    # Получить список продуктов по категории из кэша
+    products_list_by_category = cache.get('category_name')
+    # Если products_list_by_category не пустой
+    if products_list_by_category is not None:
+        # Возвратить список продуктов по категори
+        return products_list_by_category
+    # Иначе
+    else:
+        # Получить список всех продуктов по категории из базы данных
+        products_list_by_category = Product.objects.filter(category=category)
+        # Возвратить список продуктов по категории
+        return products_list_by_category
+
+
+
+
+
+
